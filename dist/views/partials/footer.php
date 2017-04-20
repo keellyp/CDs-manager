f3faa6e7cb0c3c09b9796e93398e2981458d68d5
keellyp <kelly.phan@hetic.net>
2017-04-21 00:32:23 +0200

    :tada: Initial commit
    Signed-off-by: keellyp <kelly.phan@hetic.net>

---
+++ dist/views/partials/footer.php
@@ -0,0 +1,7 @@
+        <footer>
+            Mon footer
+        </footer>
+
+    </body>
+    <script src="dist/js/script.min.js"></script>
+</html>
---
+++ dist/views/partials/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Mon template</title>
+        <link rel="stylesheet" href="/dist/css/style.min.css">
+    </head>
+    <body>
+
+        <header>
+            Mon header
+        </header>
